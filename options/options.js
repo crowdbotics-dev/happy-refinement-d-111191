263868333562a05f6e5f4dd5f1bcb8d0c9568894
Crowdbotics <engineering@crowdbotics.com>
2023-08-24 15:44:29 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "happy-refinement-d-111191",
+  url: "https://happy-refinement-d-111191.botics.co",
+  api: "https://happy-refinement-d-111191.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
